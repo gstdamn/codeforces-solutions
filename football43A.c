eea5b2044367abb2d6e6351007852c6fd0a1ad16
gustavo <gustavosantosdocarmo22@gmail.com>
2026-01-04 21:41:19 -0300

    Solution of the exercise 'Football 43 A'

--- football43A.c
+++ football43A.c
@@ -1,25 +1,21 @@
 #include <stdio.h>
-#include <string.h>
-int main()
-{
+#include <string.h>     
+int main() {
     int n;
     scanf("%d", &n);
-
-    char team1[20], team2[20];
-    scanf("%s", team1);
-
+     
+    char team1[11], team2[11], current[11];
+        scanf("%s", team1);
     int goal1 = 1, goal2 = 0;
 
-    for (int i = 1; i < n; i++){
-        scanf("%19s", team2);
-        if (strcmp(team1, team2) == 0){
-            goal1++;
+    for (int i = 1; i < n; i++) {
+        scanf("%s", current);
+        if(strcmp(current, team1) == 0) {
+                goal1++;
+        }else{
+            if(goal2 == 0) {strcpy(team2, current);}
+                goal2++;
+            }
         }
-        else{
-            if(goal2 == 0){scanf("%s" , team2);}
-            goal2++;
-        }
-    }
     printf("%s\n", (goal1 > goal2) ? team1 : team2);
-
 }
